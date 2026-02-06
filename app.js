b52ce51a2c2e8e13614da8ca69a816d7b555fdd6
Ivan Arrieta <ivanarrieta243@gmail.com>
2026-02-05 19:05:53 -0600

    Se agrego otra forma de hacerlo

--- app.js
+++ app.js
@@ -4,18 +4,28 @@ let GradosC = Number(prompt("Dame un número"));
 let GradosF;
 let GradosK;
 
-    while(true){
-    if(isNaN(GradosC)){
-        alert('No me diste un número, dame un numero por favor')
-        GradosC = Number(prompt("Dame un número"));
-    }else{
-        GradosK = (GradosC + 273.15);
-        GradosF = (GradosC * 1.8) + 32;
-        console.log(`Tenias como grado Celcius ${GradosC} pero al al convertirlo a grados Kelvin tenemos ${GradosK}`);
-        console.log(`Tenias como grado Celcius ${GradosC} pero al al convertirlo a grados Kelvin tenemos ${GradosF}`);
-        break;
-}
-    }
+//     while(true){
+//     if(isNaN(GradosC)){
+//         alert('No me diste un número, dame un numero por favor')
+//         GradosC = Number(prompt("Dame un número"));
+//     }else{
+//         GradosK = (GradosC + 273.15);
+//         GradosF = (GradosC * 1.8) + 32;
+//         console.log(`Tenias como grado Celcius ${GradosC} pero al al convertirlo a grados Kelvin tenemos ${GradosK}`);
+//         console.log(`Tenias como grado Celcius ${GradosC} pero al al convertirlo a grados Fahrenheit tenemos ${GradosF}`);
+//         break;
+// }
+//     }
 
 
     //Segundo intento de hacerlo bien
+
+
+
+    while(isNaN(GradosC)){
+        GradosC = Number(prompt("Dame un número"));
+    }
+    GradosK = (GradosC + 273.15);
+        GradosF = (GradosC * 1.8) + 32;
+        console.log(`Tenias como grado Celcius ${GradosC} pero al al convertirlo a grados Kelvin tenemos ${GradosK}`);
+        console.log(`Tenias como grado Celcius ${GradosC} pero al al convertirlo a grados Fahrenheit tenemos ${GradosF}`);
