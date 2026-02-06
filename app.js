932cca6bc06c4d6f0f075e834fc12a3cbf00b5bc
Ivan Arrieta <ivanarrieta243@gmail.com>
2026-02-05 18:57:26 -0600

    Primer intento de hacerlo, funciona, pero no creo que sea una gran solución

---
+++ app.js
@@ -0,0 +1,21 @@
+console.log("Hola Mundo, sirve para saber que me conecto")
+//Funciona, pero no me gusta del todo, es el primer intento de hacerlo
+let GradosC = Number(prompt("Dame un número"));
+let GradosF;
+let GradosK;
+
+    while(true){
+    if(isNaN(GradosC)){
+        alert('No me diste un número, dame un numero por favor')
+        GradosC = Number(prompt("Dame un número"));
+    }else{
+        GradosK = (GradosC + 273.15);
+        GradosF = (GradosC * 1.8) + 32;
+        console.log(`Tenias como grado Celcius ${GradosC} pero al al convertirlo a grados Kelvin tenemos ${GradosK}`);
+        console.log(`Tenias como grado Celcius ${GradosC} pero al al convertirlo a grados Kelvin tenemos ${GradosF}`);
+        break;
+}
+    }
+
+
+    //Segundo intento de hacerlo bien
